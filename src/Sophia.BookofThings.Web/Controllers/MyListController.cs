5f3e0b79dc2826b2de2955804b409cc49b100c33
Brooke-Estes <brooke@estes-family.net>
2016-07-10 22:04:35 -0500

    Added MyListController and index views

---
+++ src/Sophia.BookofThings.Web/Controllers/MyListController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Sophia.BookofThings.Web.Controllers
+{
+    public class MyListController : Controller
+    {
+        // GET: MyList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
